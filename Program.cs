8916eaab0ba0fe90fd4289f8b7fd13f6d99bb92b
Mahesh Misal <maheshmisal2018@gmail.com>
2024-05-07 20:45:33 +0530

    My First Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using IMS.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // Configure Entity Framework and the InventoryContext
+            builder.Services.AddDbContext<InventoryContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'CustomerContext' not found.")));
+            
+            
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Products}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
